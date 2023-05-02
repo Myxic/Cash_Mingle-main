7b72962b5d40d24df2807aff841948746dc23b22
Ezemenahi Benedict <ezemenahi56@gmail.com>
2023-05-02 23:43:17 +0100

    testing how the angular works
    - adding routes and html

--- src/app/pages/customers/customers/customers.component.ts
+++ src/app/pages/customers/customers/customers.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  styleUrls: ['./customers.component.css', '../../../../assets/css/main.css']
 })
 export class CustomersComponent {
 
--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css','../../../../assets/css/main2.css'],
 })
+
 export class LoginComponent {
+
 
 }
